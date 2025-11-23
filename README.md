5408140dd907204f4eb8c5ed6bcaa9b1641ffc63
jhbertelli <joaohenriquebertelli10@gmail.com>
2025-11-23 19:06:10 -0300

    add swagger for debug

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Ludome
+
+Swagger: https://localhost:8081/swagger/index.html
+React: http://localhost:5173/
--- src/Ludome.Server/Program.cs
+++ src/Ludome.Server/Program.cs
@@ -4,18 +4,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-app.UseDefaultFiles();
-app.MapStaticAssets();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseDefaultFiles();
+app.MapStaticAssets();
 
 app.UseHttpsRedirection();
 
